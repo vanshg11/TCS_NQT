f6514f750d320d6da91b05ea6275587c6f2c5776
vanshg11 <vanshvijay038@gmail.com>
2024-09-19 02:14:35 +0530

    sumfirst update

---
+++ Numbers/sumoffirstnaturalnumbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+void solvesumfirst(int n)
+{
+    int sum = n * (n + 1) / 2;
+    cout << sum << endl;
+}
+int main()
+{
+    int n;
+    cin >> n;
+    solvesumfirst(n);
+    return 0;
+}
